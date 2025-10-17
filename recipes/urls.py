167c95f86893eebf7da3b6c0eeb47b4cb8abf6f0
garrett-bengtson <garrett.bengtson@outlook.com>
2025-10-17 14:43:13 -0500

    Rudimentary viewing and creating recipes added. Update recipe is in progress and deleting recipe needs to be done.

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.recipes, name='recipes'),
     path('search/', views.searchRecipes, name='searchRecipes'),
+    path('addRecipe/', views.addRecipe, name='addRecipe'),
+    path('updateRecipe/', views.updateRecipe, name='updateRecipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,68 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Recipe
 
 def recipes(request):
-    template = loader.get_template('recipes.html')
-    return HttpResponse(template.render())
+    recipes = Recipe.objects.all().values()
+    context = {
+        'recipes': recipes,
+    }
+    return render(request, 'recipes.html', context)
 
 def searchRecipes(request):
-    template = loader.get_template('search.html')
-    return HttpResponse(template.render())
+    return render(request, 'search.html')
+
+def addRecipe(request):
+    if request.method == 'POST':
+        dishName = request.POST.get('dishName', '').strip()
+        creatorName = request.POST.get('creatorName', '').strip()
+        ingredients = request.POST.getlist('ingredients')
+        # strip whitespace and remove empty strings
+        ingredients = [i.strip() for i in ingredients if i and i.strip()]
+        try:
+            estimatedTime = int(request.POST.get('estimatedTime', 0))
+        except ValueError:
+            estimatedTime = 0
+        directions = request.POST.get('directions', '').strip()
+
+        # add recipe to database
+        Recipe.objects.create(
+            dishName=dishName,
+            creatorName=creatorName,
+            ingredients=ingredients,
+            estimatedTime=estimatedTime,
+            directions=directions,
+        )
+
+        return redirect('recipes')
+    return render(request, 'addRecipe.html')
+
+def updateRecipe(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    if request.method == 'POST':
+        dishName = request.POST.get('dishName', '').strip()
+        creatorName = request.POST.get('creatorName', '').strip()
+        ingredients = request.POST.getlist('ingredients')
+        # strip whitespace and remove empty strings
+        ingredients = [i.strip() for i in ingredients if i and i.strip()]
+        try:
+            estimatedTime = int(request.POST.get('estimatedTime', 0))
+        except ValueError:
+            estimatedTime = 0
+        directions = request.POST.get('directions', '').strip()
+
+        # update recipe and save to database
+        recipe.dishName = dishName
+        recipe.creatorName = creatorName
+        recipe.ingredients = ingredients
+        recipe.estimatedTime = estimatedTime
+        recipe.directions = directions
+        recipe.save()
+
+        return redirect('recipes')
+    
+    context = {
+        'recipe': recipe
+    }
+    return render(request, 'updateRecipe.html', context)
